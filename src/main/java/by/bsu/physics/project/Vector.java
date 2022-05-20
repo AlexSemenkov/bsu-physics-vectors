2c8460f2f8486ebe5af146420ab1d1452e076899
aliaksandr.semiankou <aliaksandr.semiankou@joinmosaic.com>
2022-05-20 23:05:31 +0300

    Initial commit

---
+++ src/main/java/by/bsu/physics/project/Vector.java
@@ -0,0 +1,15 @@
+package by.bsu.physics.project;
+
+/**
+ * @author asemenkov
+ * @since Oct 07, 2020
+ */
+public abstract class Vector {
+
+    public abstract double getLength();
+
+    public abstract double getScalarProduct(Vector vector);
+
+    public abstract double getCosAngle(Vector vector);
+
+}
