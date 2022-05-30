8835bbf789653b61fe48bb97a81d60c416e7884f
Stepablood <stepuravlad2002@gmail.com>
2022-05-31 01:38:25 +0300

    stepura

---
+++ src/main/java/by/bsu/physics/stepura/Coordinate.java
@@ -0,0 +1,12 @@
+package by.bsu.physics.stepura;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Coordinate {
+    int value() default -1;
+}
