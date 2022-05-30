ac6c32a18f7c5564fbe0fa7d56e135836aca70ce
beckykasss <beckysqkas@gmail.com>
2022-05-30 19:55:48 +0300

    new commit!

---
+++ src/main/java/by/bsu/physics/shust/ThreeCoordVector.java
@@ -0,0 +1,16 @@
+package by.bsu.physics.shust;
+
+public class ThreeCoordVector extends AnnotatedVector{
+    @Coordinate(0)
+    double x;
+    @Coordinate(1)
+    double y;
+    @Coordinate(2)
+    double z;
+
+    public ThreeCoordVector(double x, double y, double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+}
